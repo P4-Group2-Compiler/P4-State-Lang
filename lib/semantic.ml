eb7b1d8188f9ab46c660ce26a8383ac4aa740aab
HedinnSA <harnar24@student.aau.dk>
2026-04-10 13:50:45 +0200

    Printing for transitions in semantic.ml

--- lib/semantic.ml
+++ lib/semantic.ml
@@ -4,15 +4,16 @@ exception Semantic_error of string
 
 let error msg = raise (Semantic_error msg)
 
-
-
-type transition = state * event * state
+let state_to_string = function
+  | State s -> s
+let event_to_string = function
+  | Event e -> e
 
 let collect_states (p : program) : state list =
   List.map (fun state_decl -> state_decl.name) p.states
 
 (* Function for adding the source state with the states: (event, state) -> (state, event, state) *)
-let add_source_state (st_decl : state_decl) : transition list = 
+let add_source_state (st_decl : state_decl) : (state * event * state) list = 
   let src = st_decl.name in
   List.map (fun trans -> 
     match trans with
@@ -20,8 +21,18 @@ let add_source_state (st_decl : state_decl) : transition list =
   st_decl.transitions
 
 (* Collecting all the transitions using the add_source_state function: List of all (state, event, state)*)
-let collect_transitions (p : program) : transition list = 
+let collect_transitions (p : program) : (state * event * state) list = 
   List.concat (List.map add_source_state p.states)
+
+(* Printing the transitions - used for debugging and checking if it is correct *)
+let print_transition (src, event, dest) =
+  Printf.printf "%s --%s--> %s\n" 
+  (state_to_string src)
+  (event_to_string event)
+  (state_to_string dest)
+
+let print_iter_trans (t: (state * event * state) list) = 
+  List.iter print_transition t;
 
 
 
