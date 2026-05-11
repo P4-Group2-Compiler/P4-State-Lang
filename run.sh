8b70c98cde0209b68e82fbc6dda6256dbfafd546
HedinnSA <harnar24@student.aau.dk>
2026-05-11 13:43:55 +0200

    Enhance run.sh with cleanup and build commands
    Updated run.sh to include additional cleanup and build steps.
    
    Deletes the previously generated output files before compiling again.

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+rm -f output/c/generated_state_machine.c
+rm -f output/c/StateMachine
+rm -f output/DOT/graph.gv
+rm -f output/DOT/graph.png
+
 dune clean
-dune build 
+dune build
+
 cd bin
 dune exec ./main.exe -- test.sm
-cd .. 
-cd output/c
+
+cd ../output/c
 gcc generated_state_machine.c -o StateMachine
 ./StateMachine
