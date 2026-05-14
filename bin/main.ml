5cdee5facadc2bd2b14271a04e5cd381ddd5b4ba
HedinnSA <harnar24@student.aau.dk>
2026-05-14 11:51:58 +0200

    Fixed typos in the NoFinalState print string

--- bin/main.ml
+++ bin/main.ml
@@ -87,7 +87,7 @@ let print_warning (statemachine, warning) =
       Printf.printf "WARNING {UnreachableFinalState}:\n\tStart State: \"%s\" is unable to reach any Final State\n"
         (string_of_state state)
   | NoFinalState ->
-      Printf.printf "WARNING {NoFinalState}:\n\tNo Final State has ben declared - No input sequeence will be accepted\n"
+      Printf.printf "WARNING {NoFinalState}:\n\tNo Final State has been declared - No input sequence will be accepted\n"
   end
 
 (***)
