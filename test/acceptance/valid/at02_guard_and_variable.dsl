dabcd3094c4a7c4c11b889346e08b0b6490447ca
Peter <badehat@gmail.com>
2026-05-18 16:24:49 +0200

    Done working in this branch, will create my own from this point

--- test/acceptance/valid/at02_guard_and_variable.dsl
+++ test/acceptance/valid/at02_guard_and_variable.dsl
@@ -1,15 +1,8 @@
 Statemachine M {
-<<<<<<< HEAD
   # VAR x = 5
 
   Start State A {
     ON Push IF x > 10 GO B
-=======
-  #VAR x = 5
-
-  Start State A {
-    ON 1 IF x > 10 GO B ELSE GO C
->>>>>>> dev
   }
 
   Final State B {
--- test/acceptance_runner.ml
+++ test/acceptance_runner.ml
@@ -11,9 +11,6 @@ let parse_and_check_file filename =
     let ast = Parser.prog Lexer.token lexbuf in
     close_in chan;
 
-    (*Typechecker.type_program ast;
-    ignore (Semantic.collect_transitions ast);
-    ignore (Semantic.analyse ast);*)
     let (sm, _warnings) = Semantic.analyse ast in
     Typechecker.type_program sm;
     
@@ -57,10 +54,10 @@ let expect_warning filename () =
 let valid_tests =
   [
     Alcotest.test_case "AT-01 minimal valid state machine" `Quick
-      (expect_accepted "test/acceptance/valid/at01_minimal.dsl");
+      (expect_accepted "acceptance/valid/at01_start_final_state.dsl");
 
     Alcotest.test_case "AT-02 valid machine with guard and variable" `Quick
-      (expect_accepted "test/acceptance/valid/at02_guard_and_variable.dsl");
+      (expect_accepted "acceptance/valid/at02_guard_and_variable.dsl");
 
     (* Alcotest.test_case "AT-03 implicit transition behavior" `Quick
       (expect_accepted "test/acceptance/valid/at03_implicit_transition.dsl"); Needs ELSE functionality in guards to work *) 
@@ -69,13 +66,13 @@ let valid_tests =
 let invalid_tests =
   [
     Alcotest.test_case "AT-04 missing start state" `Quick
-      (expect_rejected "test/acceptance/invalid/at04_missing_start.dsl");
+      (expect_rejected "acceptance/invalid/at04_missing_start.dsl");
 
     Alcotest.test_case "AT-05 missing statemachine block" `Quick
-      (expect_rejected "test/acceptance/invalid/at05_missing_statemachine_block.dsl");
+      (expect_rejected "acceptance/invalid/at05_missing_statemachine_block.dsl");
 
     Alcotest.test_case "AT-06 transition to undefined state" `Quick
-      (expect_rejected "test/acceptance/invalid/at06_transition_to_undefined_state.dsl");
+      (expect_rejected "acceptance/invalid/at06_transition_to_undefined_state.dsl");
   ]
 
 let warning_tests =
@@ -84,7 +81,7 @@ let warning_tests =
       (expect_warning "test/acceptance/warning/at07_duplicate_transition.dsl"); Right now duplicate transitions are handled as semantic errors*)
 
     Alcotest.test_case "AT-08 unreachable final state" `Quick
-      (expect_warning "test/acceptance/warning/at08_unreachable_final_state.dsl");
+      (expect_warning "acceptance/warning/at08_unreachable_final_state.dsl");
   ]  
 
 let () =
