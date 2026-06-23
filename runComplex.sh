65cc45af251d7b6836c3ac0d9f4cf093c0ce7e2e
Peter <badehat@gmail.com>
2026-06-23 09:23:52 +0200

    Added Silkes demos

---
+++ runComplex.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+rm -f output/c/generated_state_machine.c
+rm -f output/c/StateMachine
+rm -f output/DOT/graph.gv
+rm -f output/DOT/graph.png
+
+dune clean
+dune build
+
+dune exec bin/main.exe -- bin/demoComplex.sm
+
+cd output/c
+gcc generated_state_machine.c -o StateMachine
+./StateMachine
